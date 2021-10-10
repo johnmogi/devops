06c5348491c5827244e82efb8fa59e6310129218
johnmogi <anguru@gmail.com>
2021-10-10 18:03:51 +0300

    script

---
+++ test/helloScript.sh
@@ -0,0 +1,6 @@
+# cat > file.txt
+cat >> file.txt
+cat << creative
+sample text
+another line
+creative
---
+++ test/helloScript2.sh
@@ -0,0 +1,7 @@
+count=5
+if (( $count > 7 )) 
+then
+    echo "the condition is true"
+else
+    echo "the condition is false"
+fi
